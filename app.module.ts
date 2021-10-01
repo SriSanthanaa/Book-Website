9174681c51514f2262914943b0fb94bd691ac467
SriSanthanaa <72208046+SriSanthanaa@users.noreply.github.com>
2021-10-01 16:15:30 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+
+/* Routing */
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+
+/* Angular Material */
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+/* FormsModule */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+/* Angular Flex Layout */
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+/* Components */
+import { LogInComponent } from './components/log-in/log-in.component';
+import { RegisterComponent } from './components/register/register.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogInComponent,
+    RegisterComponent,
+    RegisterComponent,
+    LogInComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+
+export class AppModule { }
